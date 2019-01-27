a25262882f0a330d93124bff3a7aae0efbc65c5c
LaPree1 <elhubba@pointpark.edu>
2019-01-27 18:37:39 -0500

    Jquery Bar
    Adding the bar with sample code

--- jquery_jscpt.js
+++ jquery_jscpt.js
@@ -4,3 +4,13 @@ $(document).ready(function() {
     $("img").hide();
   });
 });
+
+$('nav li ul').hide().removeClass('fallback');
+$('nav li').hover(
+  function () {
+    $('ul', this).stop().slideDown(100);
+  },
+  function () {
+    $('ul', this).stop().slideUp(100);
+  }
+);
