73d1c5b39749157d241b85c20fb6d52a04708054
LaPree1 <elhubba@pointpark.edu>
2019-01-27 17:02:25 -0500

    Create a JQ JS file
    Moving JS and Jquery to another page hoping to clean thing up.

---
+++ jquery_jscpt.js
@@ -0,0 +1,10 @@
+/* Adding things for the Jquery stuff */
+<script> /* Opening the Script */
+
+
+$("button#b1").on("click",function(){
+  $("img").hide();
+});
+
+
+</script> /*Closing the script*/
